2b11aa768df5fc1a2eab9edff40860b8ac03f30e
Mert <54467300+MertAkdag@users.noreply.github.com>
2020-08-06 07:16:59 +0300

    Add files via upload

---
+++ AdsGecis.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using GoogleMobileAds;
+using GoogleMobileAds.Api;
+
+public class AdsGecis : MonoBehaviour
+{
+
+    private InterstitialAd inter;
+
+    public string id_Android = "ca-app-pub-9457289458053843/4826625230";
+    public void Start()
+    {
+        this.Request();
+    }
+
+    private void Request()
+    {
+#if UNITY_ANDROID
+            string id = id_Android;
+#else
+
+        string id = "unexpected_paltform";
+
+#endif
+        this.inter = new InterstitialAd(id);
+
+        this.inter.OnAdClosed += InterstitialClosed;
+
+        AdRequest request = new AdRequest.Builder().Build();
+        this.inter.LoadAd(request);
+    }
+
+    private void InterstitialClosed(object sender, EventArgs e)
+    {
+        this.Request();
+    }
+    public void Show()
+    {
+        this.inter.Show();
+    }
+}
